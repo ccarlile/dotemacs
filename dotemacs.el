528ad5d2c6ead4fe1e85027eac6d9997fc17d007
Chris Carlile <christopher.carlile@gmail.com>
2018-09-19 15:16:04 -0500

    Move custom file out

--- dotemacs.el
+++ dotemacs.el
@@ -25,21 +25,6 @@
 (require 'mine-evil)
 (require 'mine-majors)
 
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
- '(package-selected-packages
-   (quote
-    (md4rd elfeed window-purpose xresources-theme dashboard evil-iedit iedit evil-avy avy smart-mode-line helm-themes golden-ratio evil-matchit evil-commentary solarized-theme dumb-jump browse-at-remote evil-magit ensime linum-relative helm-projectile evil-collection magit smartparens evil-escape zenburn-theme evil use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
